6bfb7aafa16e380f75e9dd6d6923c414a3b272a1
David Hayes <davidlynnhayes@att.net>
2015-08-30 10:37:59 -0500

    lost cors

--- config/application.rb
+++ config/application.rb
@@ -26,21 +26,21 @@ module Project3
 
     # Support Cross-Origin Resource Sharing with rack-cors gem
 
-  config.middleware.insert_before 0, "Rack::Cors" do
-    allow do
-      origins '*'
-
-      resource '/cors',
-       :headers => :any,
-       :methods => [:get, :post, :delete, :put, :patch, :options, :head],
-       :max_age => 0
-
-       resource '*',
-       :headers => :any,
-       :methods => [:get, :post, :delete, :put, :patch, :options, :head],
-       :max_age => 0
-    end
-  end
+  # config.middleware.insert_before 0, "Rack::Cors" do
+  #   allow do
+  #     origins '*'
+  #
+  #     resource '/cors',
+  #      :headers => :any,
+  #      :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+  #      :max_age => 0
+  #
+  #      resource '*',
+  #      :headers => :any,
+  #      :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+  #      :max_age => 0
+  #   end
+  # end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
